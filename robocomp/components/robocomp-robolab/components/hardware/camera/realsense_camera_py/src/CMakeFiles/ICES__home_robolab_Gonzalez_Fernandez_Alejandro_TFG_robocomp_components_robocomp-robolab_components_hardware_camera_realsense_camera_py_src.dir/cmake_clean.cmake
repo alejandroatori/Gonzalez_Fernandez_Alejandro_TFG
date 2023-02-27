2592ccfda679b59f71daeac5be1096aff17629c3
alejandroatori <agonzalegad@alumnos.unex.es>
2023-02-27 11:33:43 +0100

    D6

---
+++ robocomp/components/robocomp-robolab/components/hardware/camera/realsense_camera_py/src/CMakeFiles/ICES__home_robolab_Gonzalez_Fernandez_Alejandro_TFG_robocomp_components_robocomp-robolab_components_hardware_camera_realsense_camera_py_src.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ICES__home_robolab_Gonzalez_Fernandez_Alejandro_TFG_robocomp_components_robocomp-robolab_components_hardware_camera_realsense_camera_py_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ robocomp/components/robocomp-robolab/components/hardware/camera/realsense_camera_py/src/CMakeFiles/ICES__home_robolab_Gonzalez_Fernandez_Alejandro_TFG_robocomp_components_robocomp-robolab_components_hardware_camera_realsense_camera_py_src.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for custom commands dependencies management for ICES__home_robolab_Gonzalez_Fernandez_Alejandro_TFG_robocomp_components_robocomp-robolab_components_hardware_camera_realsense_camera_py_src.
